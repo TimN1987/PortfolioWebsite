3cddef10bea4a52acb2497088f23c57202df89d8
TimN1987 <timothy.naish@gmail.com>
2025-11-06 21:40:00 +0000

    Created home page.

--- PortfolioWebsite/Program.cs
+++ PortfolioWebsite/Program.cs
@@ -8,4 +8,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Configuration
+    .AddJsonFile("configuration.local.json", optional: true, reloadOnChange: true);
+
 await builder.Build().RunAsync();
